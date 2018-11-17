9be49f2602bd92ad39d2009ef8624fd477c18d91
Abbas <abbas1920@gmail.com>
2018-11-17 20:17:20 +0530

    Implemented contact us form validation

--- assets/js/contactus.js
+++ assets/js/contactus.js
@@ -1,5 +1,45 @@
 document.getElementById("submit").addEventListener("click", submit);
+document.getElementById("closePopup").addEventListener("click", closePopup);
 
 function submit(ev) {
-    alert("hi");
+    var elName = document.getElementById("userName");
+    var elMobile = document.getElementById("userMobile");
+    var elEMail = document.getElementById("userEMail");
+
+    var errorName = document.getElementById("errorName");
+    var errorMobile = document.getElementById("errorMobile");
+    var errorEMail = document.getElementById("errorEMail");
+
+    if (!elName.validity.valid) {
+        errorName.classList.add("show");
+    } else {
+        removeClassShow(errorName);
+    }
+
+    if (!elMobile.validity.valid) {
+        errorMobile.classList.add("show");
+    } else {
+        removeClassShow(errorMobile);
+    }
+
+    if (!elEMail.validity.valid) {
+        errorEMail.classList.add("show");
+    } else {
+        removeClassShow(errorEMail);
+    }
+
+    if (elName.validity.valid && elMobile.validity.valid && elEMail.validity.valid) {
+        document.getElementById("popup").classList.add("show");
+    }
+}
+
+function removeClassShow(el) {
+    if (el.classList.contains('show'))
+        el.classList.remove("show");
+}
+
+function closePopup(ev) {
+    if (document.getElementById("popup").classList.contains('show')) {
+        document.getElementById("popup").classList.remove("show");
+    }
 }
